99c6f0e441c8e30cba86576caacba67272219763
Jose <93064976+Alme08@users.noreply.github.com>
2025-04-30 12:24:22 -0400

    draw board and set states

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
+const turns = {
+	x: 'X',
+	o: 'O',
+};
+
+const Square = ({ children, updateBoard, index }) => {
+	return <div className='square'>{children}</div>;
+};
+
 function App() {
-	return <h1>Tic Tac Toe</h1>;
+	const [board, setBoard] = useState(Array(9).fill(null));
+	const [turn, setTurn] = useState(turns.x);
+	const [winner, setWinner] = useState(null);
+
+	return (
+		<main className='board'>
+			<h1>Tic Tac Toe</h1>
+			<section className='game'>
+				{board.map((cell, index) => {
+					return (
+						<Square key={index} index={index}>
+							{board[index]}
+						</Square>
+					);
+				})}
+			</section>
+		</main>
+	);
 }
 
 export default App;
